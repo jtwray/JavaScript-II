8677faea2d830b59771be78dcaa8ab2a6dfca3e5
jtwray <tuckerwray84@gmail.com>
2019-04-19 19:36:39 -0400

    defines higher order function multiplyNums/passes in argument values/ logs their product to console

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -54,7 +54,12 @@ sumNums(1, 20, function(added) {
 
 function multiplyNums(x, y, cb) {
     // multiplyNums multiplies two numbers and passes the result to the callback.
+    return cb(x * y);
 }
+multiplyNums(10, 20, function(product) {
+    console.log(product);
+
+});
 
 function contains(item, list, cb) {
     // contains checks if an item is present inside of the given array/list.
